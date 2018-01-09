f857f722199e186463b8fd506171e63d40e38b43
yankj12 <yankj12@163.com>
2018-01-09 23:40:38 +0800

    如何显示中文， 简单来说，首先你得用一个可以使用中文的字体，宋体、黑体什么的，或者你直接用中文TTF文件，然后文字使用unicode，即u”中文的文字”这种，最后不要忘了源文件里加上一句关于文件编码的“魔法注释”（# -*- coding:utf-8 -*-），具体的可以查一下Python的编码方面的文章

--- tutorial/demo4.py
+++ tutorial/demo4.py
@@ -2,10 +2,10 @@
 # -*- coding:utf-8 -*-
 
 
-my_name = "Will McGugan"
+my_name = u"你好"
 import pygame
 pygame.init()
-my_font = pygame.font.SysFont("arial", 64)
+my_font = pygame.font.SysFont("simsunnsimsun", 64)
 name_surface = my_font.render(my_name, True, (0, 0, 0), (255, 255, 255))
 pygame.image.save(name_surface, "name.png")
 
