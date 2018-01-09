8ab9c38afb43389df5265eefddafb8b3d82c5a0a
yankj12 <yankj12@163.com>
2018-01-09 22:06:48 +0800

    demo 窗口尺寸可改变

--- tutorial/demo3.py
+++ tutorial/demo3.py
@@ -8,24 +8,27 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
-pygame.init()
-screen = pygame.display.set_mode((640, 480), 0, 32)
-background = pygame.image.load(background_image_filename).convert()
+SCREEN_SIZE = (640, 480)
 
-Fullscreen = False
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE, RESIZABLE, 32)
+
+background = pygame.image.load(background_image_filename).convert()
 
 while True:
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            exit()
-    if event.type == KEYDOWN:
-        if event.key == K_f:
-            Fullscreen = not Fullscreen
-            if Fullscreen:
-                screen = pygame.display.set_mode((640, 480), FULLSCREEN, 32)
-            else:
-                screen = pygame.display.set_mode((640, 480), 0, 32)
+    event = pygame.event.wait()
+    if event.type == QUIT:
+        exit()
+    if event.type == VIDEORESIZE:
+        SCREEN_SIZE = event.size
+        screen = pygame.display.set_mode(SCREEN_SIZE, RESIZABLE, 32)
+        pygame.display.set_caption("Window resized to " + str(event.size))
 
-    screen.blit(background, (0 ,0))
+    screen_width, screen_height = SCREEN_SIZE
+    # 这里需要重新填满窗口
+    for y in range(0, screen_height, background.get_height()):
+        for x in range(0, screen_width, background.get_width()):
+            screen.blit(background, (x, y))
+
     pygame.display.update()
