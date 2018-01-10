736b15e7c6314afd3ed34705bc2c784ba45c0546
yankj12 <yankj12@163.com>
2018-01-10 22:27:51 +0800

    颜色平滑变化 线性插值(linear interpolation) 为了找到两种颜色的中间色，我们将这第二种颜色与第一种颜色的差乘以一个0~1之间的小数，然后再加上第一种颜色就行了。如果这个数为0，结果就完全是第一种颜色；是1，结果就只剩下第二种颜色；中间的小数则会皆有两者的特色

--- pygame_demo/tutorial/demo5.py
+++ pygame_demo/tutorial/demo5.py
@@ -1,35 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# !/usr/bin/env python
 import pygame
 from pygame.locals import *
 from sys import exit
 
 pygame.init()
-
 screen = pygame.display.set_mode((640, 480), 0, 32)
 
-
-def create_scales(height):
-    red_scale_surface = pygame.surface.Surface((640, height))
-    green_scale_surface = pygame.surface.Surface((640, height))
-    blue_scale_surface = pygame.surface.Surface((640, height))
-    for x in range(640):
-        c = int((x / 640.) * 255.)
-        red = (c, 0, 0)
-        green = (0, c, 0)
-        blue = (0, 0, c)
-        line_rect = Rect(x, 0, 1, height)
-        pygame.draw.rect(red_scale_surface, red, line_rect)
-        pygame.draw.rect(green_scale_surface, green, line_rect)
-        pygame.draw.rect(blue_scale_surface, blue, line_rect)
-    return red_scale_surface, green_scale_surface, blue_scale_surface
+color1 = (221, 99, 20)
+color2 = (96, 130, 51)
+factor = 0.
 
 
-red_scale, green_scale, blue_scale = create_scales(80)
+def blend_color(color1, color2, blend_factor):
+    r1, g1, b1 = color1
+    r2, g2, b2 = color2
+    r = r1 + (r2 - r1) * blend_factor
+    g = g1 + (g2 - g1) * blend_factor
+    b = b1 + (b2 - b1) * blend_factor
+    return int(r), int(g), int(b)
 
-color = [127, 127, 127]
 
 while True:
 
@@ -37,24 +28,18 @@ while True:
         if event.type == QUIT:
             exit()
 
-    screen.fill((0, 0, 0))
+    screen.fill((255, 255, 255))
 
-    screen.blit(red_scale, (0, 00))
-    screen.blit(green_scale, (0, 80))
-    screen.blit(blue_scale, (0, 160))
+    tri = [(0, 120), (639, 100), (639, 140)]
+    pygame.draw.polygon(screen, (0, 255, 0), tri)
+    pygame.draw.circle(screen, (0, 0, 0), (int(factor * 639.0), 120), 10)
 
     x, y = pygame.mouse.get_pos()
-
     if pygame.mouse.get_pressed()[0]:
-        for component in range(3):
-            if y > component * 80 and y < (component + 1) * 80:
-                color[component] = int((x / 639.) * 255.)
-        pygame.display.set_caption("PyGame Color Test - " + str(tuple(color)))
+        factor = x / 639.0
+        pygame.display.set_caption("Pygame Color Blend Test - %.3f" % factor)
 
-    for component in range(3):
-        pos = (int((color[component] / 255.) * 639), component * 80 + 40)
-        pygame.draw.circle(screen, (255, 255, 255), pos, 20)
-
-    pygame.draw.rect(screen, tuple(color), (0, 240, 640, 240))
+    color = blend_color(color1, color2, factor)
+    pygame.draw.rect(screen, color, (0, 240, 640, 240))
 
     pygame.display.update()
