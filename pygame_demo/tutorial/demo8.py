66edfdeccf753a39dcdb5065485340ca74c01a4a
yankj12 <yankj12@163.com>
2018-01-11 22:36:20 +0800

    斜线运动

--- pygame_demo/tutorial/demo8.py
+++ pygame_demo/tutorial/demo8.py
@@ -14,14 +14,12 @@ pygame.init()
 screen = pygame.display.set_mode((640, 480), 0, 32)
 
 background = pygame.image.load(background_image_filename).convert()
-sprite = pygame.image.load(sprite_image_filename)
+sprite = pygame.image.load(sprite_image_filename).convert_alpha()
 
-# Clock对象
 clock = pygame.time.Clock()
 
-x = 0.
-# 速度（像素/秒）
-speed = 250.
+x, y = 100., 100.
+speed_x, speed_y = 133., 170.
 
 while True:
 
@@ -30,16 +28,27 @@ while True:
             exit()
 
     screen.blit(background, (0, 0))
-    screen.blit(sprite, (x, 100))
+    screen.blit(sprite, (x, y))
 
-    time_passed = clock.tick()
+    time_passed = clock.tick(30)
     time_passed_seconds = time_passed / 1000.0
 
-    distance_moved = time_passed_seconds * speed
-    x += distance_moved
+    x += speed_x * time_passed_seconds
+    y += speed_y * time_passed_seconds
 
-    # 想一下，这里减去640和直接归零有何不同？
-    if x > 640.:
-        x -= 640.
+    # 到达边界则把速度反向
+    if x > 640 - sprite.get_width():
+        speed_x = -speed_x
+        x = 640 - sprite.get_width()
+    elif x < 0:
+        speed_x = -speed_x
+        x = 0.
+
+    if y > 480 - sprite.get_height():
+        speed_y = -speed_y
+        y = 480 - sprite.get_height()
+    elif y < 0:
+        speed_y = -speed_y
+        y = 0
 
     pygame.display.update()
