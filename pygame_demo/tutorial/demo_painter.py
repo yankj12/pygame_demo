1863d749718956946baf6caae82ae022c14cedf7
yankj12 <yankj12@163.com>
2018-01-21 00:18:04 +0800

    涂鸦画板的例子，增加菜单和GUI界面

--- pygame_demo/tutorial/demo_painter.py
+++ pygame_demo/tutorial/demo_painter.py
@@ -5,6 +5,9 @@ import pygame
 from pygame.locals import *
 import math
 
+
+# 2011/08/27 Version 1, first imported
+
 class Brush():
     def __init__(self, screen):
         self.screen = screen
@@ -35,11 +38,14 @@ class Brush():
     def get_brush_style(self):
         return self.style
 
+    def get_current_brush(self):
+        return self.brush_now
+
     def set_size(self, size):
         if size < 0.5:
             size = 0.5
-        elif size > 50:
-            size = 50
+        elif size > 32:
+            size = 32
         print "* set brush size to", size
         self.size = size
         self.brush_now = self.brush.subsurface((0, 0), (size * 2, size * 2))
@@ -47,13 +53,22 @@ class Brush():
     def get_size(self):
         return self.size
 
+    def set_color(self, color):
+        self.color = color
+        for i in xrange(self.brush.get_width()):
+            for j in xrange(self.brush.get_height()):
+                self.brush.set_at((i, j),
+                                  color + (self.brush.get_at((i, j)).a,))
+
+    def get_color(self):
+        return self.color
+
     def draw(self, pos):
         if self.drawing:
             for p in self._get_points(pos):
                 # draw eveypoint between them
                 if self.style == False:
-                    pygame.draw.circle(self.screen,
-                                       self.color, p, self.size)
+                    pygame.draw.circle(self.screen, self.color, p, self.size)
                 else:
                     self.screen.blit(self.brush_now, p)
 
@@ -71,8 +86,95 @@ class Brush():
             points.append(
                 (points[-1][0] + step_x, points[-1][1] + step_y))
         points = map(lambda x: (int(0.5 + x[0]), int(0.5 + x[1])), points)
-        # return light-weight, uniq list
+        # return light-weight, uniq integer point list
         return list(set(points))
+
+
+class Menu():
+    def __init__(self, screen):
+        self.screen = screen
+        self.brush = None
+        self.colors = [
+            (0xff, 0x00, 0xff), (0x80, 0x00, 0x80),
+            (0x00, 0x00, 0xff), (0x00, 0x00, 0x80),
+            (0x00, 0xff, 0xff), (0x00, 0x80, 0x80),
+            (0x00, 0xff, 0x00), (0x00, 0x80, 0x00),
+            (0xff, 0xff, 0x00), (0x80, 0x80, 0x00),
+            (0xff, 0x00, 0x00), (0x80, 0x00, 0x00),
+            (0xc0, 0xc0, 0xc0), (0xff, 0xff, 0xff),
+            (0x00, 0x00, 0x00), (0x80, 0x80, 0x80),
+        ]
+        self.colors_rect = []
+        for (i, rgb) in enumerate(self.colors):
+            rect = pygame.Rect(10 + i % 2 * 32, 254 + i / 2 * 32, 32, 32)
+            self.colors_rect.append(rect)
+
+        self.pens = [
+            pygame.image.load("pen1.png").convert_alpha(),
+            pygame.image.load("pen2.png").convert_alpha()
+        ]
+        self.pens_rect = []
+        for (i, img) in enumerate(self.pens):
+            rect = pygame.Rect(10, 10 + i * 64, 64, 64)
+            self.pens_rect.append(rect)
+
+        self.sizes = [
+            pygame.image.load("big.png").convert_alpha(),
+            pygame.image.load("small.png").convert_alpha()
+        ]
+        self.sizes_rect = []
+        for (i, img) in enumerate(self.sizes):
+            rect = pygame.Rect(10 + i * 32, 138, 32, 32)
+            self.sizes_rect.append(rect)
+
+    def set_brush(self, brush):
+        self.brush = brush
+
+    def draw(self):
+        # draw pen style button
+        for (i, img) in enumerate(self.pens):
+            self.screen.blit(img, self.pens_rect[i].topleft)
+        # draw < > buttons
+        for (i, img) in enumerate(self.sizes):
+            self.screen.blit(img, self.sizes_rect[i].topleft)
+        # draw current pen / color
+        self.screen.fill((255, 255, 255), (10, 180, 64, 64))
+        pygame.draw.rect(self.screen, (0, 0, 0), (10, 180, 64, 64), 1)
+        size = self.brush.get_size()
+        x = 10 + 32
+        y = 180 + 32
+        if self.brush.get_brush_style():
+            x = x - size
+            y = y - size
+            self.screen.blit(self.brush.get_current_brush(), (x, y))
+        else:
+            print (x, y), ' ', size
+            pygame.draw.circle(self.screen,
+                               self.brush.get_color(), (x, y), size)
+        # draw colors panel
+        for (i, rgb) in enumerate(self.colors):
+            pygame.draw.rect(self.screen, rgb, self.colors_rect[i])
+
+    def click_button(self, pos):
+        # pen buttons
+        for (i, rect) in enumerate(self.pens_rect):
+            if rect.collidepoint(pos):
+                self.brush.set_brush_style(bool(i))
+                return True
+        # size buttons
+        for (i, rect) in enumerate(self.sizes_rect):
+            if rect.collidepoint(pos):
+                if i:  # i == 1, size down
+                    self.brush.set_size(self.brush.get_size() - 1)
+                else:
+                    self.brush.set_size(self.brush.get_size() + 1)
+                return True
+        # color buttons
+        for (i, rect) in enumerate(self.colors_rect):
+            if rect.collidepoint(pos):
+                self.brush.set_color(self.colors[i])
+                return True
+        return False
 
 
 class Painter():
@@ -81,6 +183,8 @@ class Painter():
         pygame.display.set_caption("Painter")
         self.clock = pygame.time.Clock()
         self.brush = Brush(self.screen)
+        self.menu = Menu(self.screen)
+        self.menu.set_brush(self.brush)
 
     def run(self):
         self.screen.fill((255, 255, 255))
@@ -95,12 +199,19 @@ class Painter():
                     if event.key == K_ESCAPE:
                         self.screen.fill((255, 255, 255))
                 elif event.type == MOUSEBUTTONDOWN:
-                    self.brush.start_draw(event.pos)
+                    # <= 74, coarse judge here can save much time
+                    if ((event.pos)[0] <= 74 and
+                            self.menu.click_button(event.pos)):
+                        # if not click on a functional button, do drawing
+                        pass
+                    else:
+                        self.brush.start_draw(event.pos)
                 elif event.type == MOUSEMOTION:
                     self.brush.draw(event.pos)
                 elif event.type == MOUSEBUTTONUP:
                     self.brush.end_draw()
 
+            self.menu.draw()
             pygame.display.update()
 
 
