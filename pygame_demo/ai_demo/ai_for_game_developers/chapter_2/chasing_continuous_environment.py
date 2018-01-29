07370a02c178456c8b7b25bfbc0d982a1ed8f9d8
yankj12 <yankj12@163.com>
2018-01-29 15:54:33 +0800

    画出小船

--- pygame_demo/ai_demo/ai_for_game_developers/chapter_2/chasing_continuous_environment.py
+++ pygame_demo/ai_demo/ai_for_game_developers/chapter_2/chasing_continuous_environment.py
@@ -37,19 +37,64 @@ class Boat(object):
 
 
 
+boat1 = Boat(100, 100, 0, 40, 20)
+
 def draw_boat(screen, boat):
-    # 绘制一个圆形
-    position = ball.x, ball.y
-    radius = square_width / 2
+    # 绘制多边形
+    center_x = boat.center_x
+    center_y = boat.center_y
+    boat_length = boat.boat_length
+    boat_width = boat.boat_width
+
     width = 1
-    color = ball.color
+    color = boat.color
+
+    #boat_length边与x轴的夹角
+    angle_degrees = boat.direction
+
+    #对角线
+    diagonal = math.sqrt(boat_length ** 2 + boat_width ** 2)
+    #对角线与boat_length边的夹角
+    #弧度
+    angle1_radians = math.atan(boat_width * 1.0/boat_length)
+    angle1_degrees = math.degrees(angle1_radians)
+    #对角线与x轴夹角
+    angle2_degrees = angle_degrees - angle1_degrees
+
+    p4 = {}
+    p4['x'] = center_x + diagonal * 0.5 * math.cos(math.radians(angle2_degrees))
+    p4['y'] = center_y + diagonal * 0.5 * math.sin(math.radians(angle2_degrees))
+
+    angle3_degrees = angle_degrees + angle1_degrees
+    p2 = {}
+    p2['x'] = center_x + diagonal * 0.5 * math.cos(math.radians(angle3_degrees))
+    p2['y'] = center_y + diagonal * 0.5 * math.sin(math.radians(angle3_degrees))
+
+    p3 = {}
+    p3['x'] = center_x + (boat_length + boat_width) * 0.5 * math.cos(math.radians(angle_degrees))
+    p3['y'] = center_y + (boat_length + boat_width) * 0.5 * math.sin(math.radians(angle_degrees))
+
+    # 对称性
+    p1 = {}
+    p1['x'] = center_x * 2 - p4['x']
+    p1['y'] = center_y * 2 - p4['y']
+
+    p5 = {}
+    p5['x'] = center_x * 2 - p2['x']
+    p5['y'] = center_y * 2 - p2['y']
 
     # pygame.draw.circle
     # 原型：pygame.draw.circle(Surface, color, pos, radius, width=0): return Rect
     # 当不传入width参数的时候，绘制的是一个填充了的圆
-    pygame.draw.circle(screen, color, position, radius)
-
-
+    #pygame.draw.circle(screen, color, position, radius)
+    # Rect(left,top,width,height)用来定义位置和宽高
+    #pygame.draw.rect(screen, color, [p1['x'], p1['y'], boat_length, boat_width], 0)
+    # polygon存在锯齿
+    #pygame.draw.polygon(screen, color, [(p1['x'], p1['y']), (p2['x'], p2['y']), (p3['x'], p3['y']), (p4['x'], p4['y']), (p5['x'], p5['y'])], 0)
+    # 消除锯齿
+    pygame.draw.aalines(screen, color, True, [(p1['x'], p1['y']), (p2['x'], p2['y']), (p3['x'], p3['y']), (p4['x'], p4['y']), (p5['x'], p5['y'])], 1)
+    # 画出中心
+    pygame.draw.aaline(screen, color, (center_x, center_y), (center_x, center_y), 1)
 
 # 移动向量(delta_row, delta_col)
 move = (0, 0)
@@ -85,12 +130,12 @@ while True:
             exit()
         if event.type == KEYDOWN:
             if event.key == K_LEFT:
-                # 按下的是左方向键的话，把x坐标减一
-                move = (0, -1)
+                # 船的角度
+                boat1.direction -= 2
 
             elif event.key == K_RIGHT:
-                # 右方向键则加一
-                move = (0, 1)
+                # 船的角度
+                boat1.direction += 2
 
             elif event.key == K_UP:
                 move = (-1, 0)
@@ -105,6 +150,7 @@ while True:
     # 绘制白色背景色
     screen.fill(white)
 
+    draw_boat(screen, boat1)
 
     pygame.display.update()
     sleep(0.5)
