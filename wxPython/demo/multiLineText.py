9c49e9bdcc926a6f2b6c0aafd604405d4435b3e9
yankj12 <yankj12@163.com>
2018-02-11 00:35:44 +0800

    添加菜单

--- wxPython/demo/multiLineText.py
+++ wxPython/demo/multiLineText.py
@@ -8,8 +8,20 @@ class my_frame(wx.Frame):
 
     """We simple derive a new class of frame"""
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(300, 100))
+        wx.Frame.__init__(self, parent, title=title, size=(300, 200))
+        # 继承来自wx.Frame的__init__方法。声明一个wx.TextCtrl控件
+        # （简单的文本编辑控件）
         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.Show(True)
+        # 创建窗口底部的状态栏
+        self.CreateStatusBar()
+        filemenu = wx.Menu()
+        filemenu.Append(wx.ID_EXIT, "Exit", "Exit the program.")
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_ABOUT, "About", "Information about this program.")
+        menubar = wx.MenuBar()
+        menubar.Append(filemenu, u"设置")
+        self.SetMenuBar(menubar)
         self.Show(True)
 
 
