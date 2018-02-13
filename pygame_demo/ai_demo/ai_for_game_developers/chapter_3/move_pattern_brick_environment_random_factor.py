fcba76e5f3b923b2a4e0b2ce743e43e13b8b9a41
yankj12 <yankj12@163.com>
2018-02-13 10:11:08 +0800

    修复随机运动模式下的bug，可以正确地按照运动模式进行运动

--- pygame_demo/ai_demo/ai_for_game_developers/chapter_3/move_pattern_brick_environment_random_factor.py
+++ pygame_demo/ai_demo/ai_for_game_developers/chapter_3/move_pattern_brick_environment_random_factor.py
@@ -15,7 +15,13 @@ class AiEntity(object):
     def __init__(self, k_max_rows, k_max_cols):
         self.k_max_rows = k_max_rows
         self.k_max_cols = k_max_cols
-        self.pattern = [[0] * self.k_max_cols] * self.k_max_rows
+        # self.pattern = [[0] * self.k_max_cols] * self.k_max_rows    # 这种初始化方式始终浅层复制的方式，在此例中不符合应用要求
+        self.pattern = []
+        for i in range(0, self.k_max_rows, 1):
+            ary = []
+            for j in range(0, self.k_max_cols, 1):
+                ary.append(0)
+            self.pattern.append(ary)
 
 
     # 初始化路径
@@ -29,10 +35,10 @@ class AiEntity(object):
     def build_path_segment(self, start_point, end_point):
         current_point = PathPoint(start_point.row, start_point.col)
         self.pattern[current_point.row][current_point.col] = 1
-        delta_row = current_point.row - end_point.row
-        delta_col = current_point.col - end_point.col
+        delta_row = end_point.row - current_point.row
+        delta_col = end_point.col - current_point.col
 
-        while delta_row != 0 and delta_col != 0:
+        while delta_row != 0 or delta_col != 0:
             if math.fabs(delta_row) > math.fabs(delta_col):
                 next_row = current_point.row + int(delta_row / math.fabs(delta_row))
                 next_col = current_point.col
@@ -45,12 +51,12 @@ class AiEntity(object):
 
             current_point = PathPoint(next_row, next_col)
             self.pattern[current_point.row][current_point.col] = 1
-            delta_row = current_point.row - end_point.row
-            delta_col = current_point.col - end_point.col
+            delta_row = end_point.row - current_point.row
+            delta_col = end_point.col - current_point.col
 
 
 
-    def follow_pattern(self, record_path):
+    def follow_pattern(self, current, previous):
         possible_path = []
         possible_path.append(PathPoint(0, 0))
         possible_path.append(PathPoint(0, 0))
@@ -72,16 +78,6 @@ class AiEntity(object):
         offset.append(PathPoint(1, 0))
         offset.append(PathPoint(1, 1))
 
-        current = PathPoint(-1, -1)
-        if len(record_path) >= 1:
-            current.row = record_path[-1].row
-            current.col = record_path[-1].col
-
-        previous = PathPoint(-1, -1)
-        if len(record_path) >= 2:
-            previous.row = record_path[-2].row
-            previous.col = record_path[-2].col
-
         j = 0
         for i in range(0, 8, 1):
             # self.pattern 需要在 build_path_segment 中初始化
@@ -89,10 +85,12 @@ class AiEntity(object):
                 if not (((current.row + offset[i].row) == previous.row)
                          and ((current.col + offset[i].col) == previous.col)):
                     possible_path[j].row = current.row + offset[i].row
-                    possible_path[j].col = current.row + offset[i].col
+                    possible_path[j].col = current.col + offset[i].col
                     j += 1
-
-        i = random.randint(0 , j-1)
+        if j > 0:
+            i = random.randint(0 , j-1)
+        else:
+            i = 0
         # 如果我们记录下当前走过的所有路径，那么可以直接从记录下来的路径中查找 previous
         previous.row = current.row
         previous.col = current.col
@@ -108,10 +106,10 @@ entity.build_path_segment(PathPoint(2, 10), PathPoint(10, 10))
 entity.build_path_segment(PathPoint(10, 10), PathPoint(10, 2))
 entity.build_path_segment(PathPoint(10, 2), PathPoint(2, 2))
 
-print entity.pattern
-
 entity_list.append(entity)
 
+# 起始的点
+start_point = PathPoint(2, 2)
 
 import pygame
 from pygame.locals import *
@@ -171,7 +169,8 @@ def draw_ball(screen, ball):
 
 
 # 定义一个小球
-ball = Ball()
+# 小球的初始位置为运动的初始位置
+ball = Ball(start_point.row, start_point.col)
 
 # 上一秒的秒数
 last_second = time.time()
@@ -200,13 +199,23 @@ while True:
     current_second = time.time()
     # 每隔0.5秒移动一次
     if current_second - last_second >= 0.5:
+        current = PathPoint(start_point.row, start_point.col)
+        if len(record_path) >= 1:
+            current.row = record_path[-1].row
+            current.col = record_path[-1].col
+
+        previous = PathPoint(start_point.row, start_point.col)
+        if len(record_path) >= 2:
+            previous.row = record_path[-2].row
+            previous.col = record_path[-2].col
+
         # 移动模式
         move_pattern = entity_list[0]
-        next_point = move_pattern.follow_pattern(record_path)
+        next_point = move_pattern.follow_pattern(current, previous)
         ball.row = next_point.row
         ball.col = next_point.col
-
-        record_path.append(PathPoint(next_point.row, next_point.col))
+        #print ball.row, ',', ball.col
+        record_path.append(PathPoint(ball.row, ball.col))
 
         # 将当前毫秒数赋值给上一秒毫秒数
         last_second = current_second
